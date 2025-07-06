568f3b16bc9c460d57ab6cc747688afb4eedf9a9
Jakub Misiarz <jakub.misiarz@sap.com>
2025-07-06 18:46:25 +0200

    change viteLogo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+const viteLogo = new URL('/vite.svg', import.meta.url).href;
 import './App.css'
 
 function App() {
